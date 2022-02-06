ddaac2579322b0b17469ece14c86a22ba324c414
StevenVallarsa <orionsjv@gmail.com>
2022-02-05 21:29:10 -0500

    started adding css

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -3,11 +3,21 @@ import "./top.scss";
 export default function Top(props) {
   return (
     <div className="top">
-      <h4 className="computer">**** STEVEN VALLARSA 4.0 ****</h4>
-      <h4 className="computer">&nbsp; 15359 BYTES FREE</h4>
-      <h4 className="computer">READY...</h4>
-      <h4 className="computer">&nbsp;</h4>
-      <h2 className="computer">&nbsp; ...FOR WORK</h2>
+      <div className="wrapper">
+        <div className="left">
+          <h4>**** STEVEN VALLARSA 4.0 ****</h4>
+          <h4>&nbsp; 15359 BYTES FREE</h4>
+          <h4>READY...</h4>
+          <h2>&nbsp; ...FOR WORK</h2>
+        </div>
+        {/* <div className="center"></div> */}
+        <div className="right">
+          <p>Wild E. Coyote</p>
+          <p>
+            <span>GENIUS</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,7 @@ import "./works.scss";
 
 export default function Works() {
   return (
-    <div className="works">
+    <div className="works" id="works">
       <h1>Works</h1>
     </div>
   );
